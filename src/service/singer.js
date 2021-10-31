91537b40dab02af08bd05bda9ac7d82959796af8
zy599 <767927053@qq.com>
2021-10-31 15:31:37 +0800

    完成歌手列表布局

---
+++ src/service/singer.js
@@ -0,0 +1,4 @@
+import { get } from './base'
+export function getSingerList() {
+  return get('/api/getSingerList')
+}
--- src/views/singer.vue
+++ src/views/singer.vue
@@ -1,13 +1,35 @@
 <template>
-  <h2>歌手</h2>
+  <div class="singer" v-loading="!singerList.length">
+    <index-list :data="singerList" />
+  </div>
 </template>
 
 <script>
+import { getSingerList } from '@/service/singer'
+import indexList from '../components/base/index-list/index-list.vue'
 export default {
-  name: 'singer'
+  name: 'singer',
+  components: {
+    indexList
+  },
+  data() {
+    return {
+      singerList: []
+    }
+  },
+  async created() {
+    const result = await getSingerList()
+    this.singerList = result.singers
+  }
+
 }
 </script>
 
 <style lang="scss" scoped>
-
+  .singer{
+    position: fixed;
+    width: 100%;
+    top: 88px;
+    bottom: 0;
+  }
 </style>
