c3fc5c5f545dc308e3fb4bb96daba3df97f5ed51
zy599 <767927053@qq.com>
2022-02-18 19:29:35 +0800

    编写播放器基础样式

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <m-header />
   <m-tab />
   <router-view></router-view>
+  <MPlayer />
 </template>
 
 <script>
 import Header from '@/components/header/header.vue'
 import Tab from '@/components/tab/tab.vue'
+import Player from './components/player/player'
 export default {
   components: {
     MHeader: Header,
-    MTab: Tab
+    MTab: Tab,
+    MPlayer: Player
   }
 }
 </script>
