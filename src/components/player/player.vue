463d593684a74c4586e214b9e746aef20e72749e
codezy <767927053@qq.com>
2022-02-19 00:04:14 +0800

    播放器暂停开始

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -11,8 +11,27 @@
         <h1 class="title">{{ currentSong.name }}</h1>
         <h2 class="subtitle">{{ currentSong.singer }}</h2>
       </div>
+      <div class="bottom">
+        <div class="operators">
+          <div class="icon i-left">
+            <i class="icon-sequence"></i>
+          </div>
+          <div class="icon i-left">
+            <i class="icon-prev"></i>
+          </div>
+          <div class="icon i-center" @click="togglePlay">
+            <i :class="playIcon"></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon icon-next"></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon-not-favorite"></i>
+          </div>
+        </div>
+      </div>
     </div>
-    <audio ref="audioRef"></audio>
+    <audio ref="audioRef" @pause="pause"></audio>
   </div>
 </template>
 
@@ -27,6 +46,10 @@ export default {
     const audioRef = ref(null)
     const fullScreen = computed(() => store.state.fullScreen)
     const currentSong = computed(() => store.getters.currentSong)
+    const playing = computed(() => store.state.playing)
+    const playIcon = computed(() => {
+      return playing.value ? 'icon-pause' : 'icon-play'
+    })
     watch(currentSong, (newSong) => {
       if (!newSong.id || !newSong.url) {
         return
@@ -35,14 +58,29 @@ export default {
       audioEl.src = newSong.url
       audioEl.play()
     })
+    watch(playing, (newPlaying) => {
+      const audioEl = audioRef.value
+      newPlaying ? audioEl.play() : audioEl.pause()
+    })
     const goBack = () => {
-      store.commit('')
+      store.commit('setFullSreen', false)
+    }
+
+    const togglePlay = () => {
+      store.commit('setPlayState', !playing.value)
+    }
+
+    const pause = () => {
+      store.commit('setPlayState', false)
     }
     return {
       audioRef,
       fullScreen,
       currentSong,
-      goBack
+      goBack,
+      playIcon,
+      togglePlay,
+      pause
     }
   }
 }
@@ -111,6 +149,97 @@ export default {
         color: $color-text;
       }
     }
+    .bottom {
+        position: absolute;
+        bottom: 50px;
+        width: 100%;
+        .dot-wrapper {
+          text-align: center;
+          font-size: 0;
+          .dot {
+            display: inline-block;
+            vertical-align: middle;
+            margin: 0 4px;
+            width: 8px;
+            height: 8px;
+            border-radius: 50%;
+            background: $color-text-l;
+            &.active {
+              width: 20px;
+              border-radius: 5px;
+              background: $color-text-ll;
+            }
+          }
+        }
+        .progress-wrapper {
+          display: flex;
+          align-items: center;
+          width: 80%;
+          margin: 0px auto;
+          padding: 10px 0;
+          .time {
+            color: $color-text;
+            font-size: $font-size-small;
+            flex: 0 0 40px;
+            line-height: 30px;
+            width: 40px;
+            &.time-l {
+              text-align: left;
+            }
+            &.time-r {
+              text-align: right;
+            }
+          }
+          .progress-bar-wrapper {
+            flex: 1;
+          }
+        }
+        .operators {
+          display: flex;
+          align-items: center;
+          .icon {
+            flex: 1;
+            color: $color-theme;
+            &.disable {
+              color: $color-theme-d;
+            }
+            i {
+              font-size: 30px;
+            }
+          }
+          .i-left {
+            text-align: right;
+          }
+          .i-center {
+            padding: 0 20px;
+            text-align: center;
+            i {
+              font-size: 40px;
+            }
+          }
+          .i-right {
+            text-align: left
+          }
+          .icon-favorite {
+            color: $color-sub-theme;
+          }
+        }
+      }
+      &.normal-enter-active, &.normal-leave-active {
+        transition: all .6s;
+        .top, .bottom {
+          transition: all .6s cubic-bezier(0.45, 0, 0.55, 1);
+        }
+      }
+      &.normal-enter-from, &.normal-leave-to {
+        opacity: 0;
+        .top {
+          transform: translate3d(0, -100px, 0);
+        }
+        .bottom {
+          transform: translate3d(0, 100px, 0)
+        }
+      }
   }
 }
 </style>
